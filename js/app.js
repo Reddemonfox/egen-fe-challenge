2a152a8f90333abdca8eff8a9379e5d572023c61
himanshu.s <himanshu.s@cargoexchange.in>
2016-07-28 12:03:36 +0530

    final commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ angular.module('myApp', [])
     .controller('mainController', function ($scope) {
         $scope.isValid = false;
         $scope.clr = '#e0e0e0';
+        $scope.max_length = 19;
+        $scope.card_number = '';
         // Setting the border of input box according to the valid status of card number entered
         $scope.onBlur =function () {
             if($scope.isValid){
@@ -10,50 +12,58 @@ angular.module('myApp', [])
             else{
                 $scope.clr = '#F44336'; // red for invalid card
             }
-        }
+        };
         $scope.onFocus = function() {
                 $scope.clr = '#e0e0e0';
-        }
+        };
     })
     .filter('validate', [function () {
-        return function (ccnumber,scope) {
-            var result,valid;
-            if (!ccnumber) {
+        return function (card_number,scope) {
+            var result;
+            if (!card_number) {
                 return 'icons/d_c.png';
             }
-            ccnumber = ccnumber.toString().replace(/\s+/g, '');
-            var len = ccnumber.length;
-            var cardType;
-                mul = 0,
-                prodArr = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]],
-                sum = 0;
-            while (len--) {
-                sum += prodArr[mul][parseInt(ccnumber.charAt(len), 10)];
-                mul ^= 1;
-            }
-            //check the validity using luhn algo
-            if (sum % 10 === 0 && sum > 0) {
-                scope.isValid = true;
-            } else {
-                scope.isValid =false;
+            //restriction the input field to numbers only
+            scope.card_number = card_number.toString().replace(/\D+|\s+/g, "");
+
+            //luhn Algorithm
+            var nCheck = 0, nDigit = 0, bEven = false;
+            for (var n = card_number.length - 1; n >= 0; n--) {
+                var cDigit = card_number.charAt(n),
+                    nDigit = parseInt(cDigit, 10);
+
+                if (bEven) {
+                    if ((nDigit *= 2) > 9) nDigit -= 9;
+                }
+
+                nCheck += nDigit;
+                bEven = !bEven;
             }
 
-            if(/^(34)|^(37)/.test(ccnumber)) {
+            scope.isValid = ((nCheck % 10) == 0);
+
+            if(/^(34)|^(37)/.test(card_number)) {
+                scope.max_length = 15;
                 result = 'icons/a_e.png'; // American Express Card
             }
-            else if(/^(62)|^(88)/.test(ccnumber)) {
+            else if(/^(62)|^(88)/.test(card_number)) {
+                scope.max_length = 19;
                 result = 'icons/c_u_p.png'; //China Union Pay card
             }
-            else if(/^(5018)|^(5020)|^(5038)|^(5893)|^(6304)|^(6759)|^(6761)|^(6762)|^(6763)|^(0604)/.test(ccnumber)) {
+            else if(/^(5018)|^(5020)|^(5038)|^(5893)|^(6304)|^(6759)|^(6761)|^(6762)|^(6763)|^(0604)/.test(card_number)) {
+                scope.max_length = 19;
                 result = 'icons/m.png'; //Maestro card
             }
-            else if(/^5[1-5]/.test(ccnumber)) {
+            else if(/^5[1-5]/.test(card_number)) {
+                scope.max_length = 16;
                 result = 'icons/m_c.png'; // Master card
             }
-            else if (/^4/.test(ccnumber)) {
+            else if (/^4/.test(card_number)) {
+                scope.max_length = 19;
                 result = 'icons/v.png'; // Visa card
             }
             else{
+                scope.max_length = 19;
                 result = 'icons/d_c.png';
             }
             return result;
